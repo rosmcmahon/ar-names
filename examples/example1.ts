c682f9805f0802a6d1448c4222dc93d56e8424f2
Ros McMahon <rosmcmahon@gmail.com>
2022-02-13 09:23:26 +0700

    quick nodejs example

---
+++ examples/example1.ts
@@ -0,0 +1,15 @@
+import { Names, Account } from '../src' //or 'arid'
+
+const main = async()=> {
+	// this should probably be called on page load to cache names in advance
+	const names = await Names.getInstance()
+
+	// perhaps you can get this from your wallet provider or extract from a jwk
+	const myaddress = 'v2XXwq_FvVqH2KR4p_x8H-SQ7rDwZBbykSv-59__Avc'
+
+	let account = names.get(myaddress)
+
+	console.log('Name:', account.name)
+	console.log(`Avatar: https://arweave.net/${account.avatarTxid}`)
+}
+main()
